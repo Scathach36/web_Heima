d77089692420526c52560e7dd43a223a72e45be8
scathach <scathach0822@hotmail.com>
2022-07-16 12:09:41 +0800

    主页内容开发完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,4 +7,14 @@ $.ajaxPrefilter(function (options) {
             Authorization: localStorage.getItem('token') || ''
         }
     }
+
+    // 全局 complete
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 清空 token
+            localStorage.removeItem('token')
+            // 返回登录页面
+            location.href = '/login.html'
+        }
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -10,13 +10,36 @@ function getUserinfo() {
         //     Authorization: localStorage.getItem('token') || ''
         // },
         success: function (res) {
-            console.log(res.data);
+            // console.log(res.data);
             if (res.status !== 0) {
                 return layui.layer.msg('获取用户信息失败！')
             }
             // 渲染用户头像
             renderAvatar(res.data)
         }
+        // 无论请求成功与否，都会执行 complete 中的内容
+        // complete: function (res) {
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         // 清空 token
+        //         localStorage.removeItem('token')
+        //         // 返回登录页面
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+
+    // 用户退出
+    $('#btn-Logout').on('click', function () {
+        let layer = layui.layer
+        // 弹出提示框
+        layer.confirm('是否要退出?', { icon: 3, title: '提示' }, function (index) {
+            // 清空 token
+            localStorage.removeItem('token')
+            // 跳转回登录页
+            location.href = '/login.html'
+
+            layer.close(index);
+        });
     })
 }
 
